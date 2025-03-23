f03a1436a2d0bb44d3c93b708274486387ea3057
RadosavPanic <radosav.panic01@gmail.com>
2025-03-23 06:47:02 +0100

    vercel deployment fixes

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -59,12 +59,16 @@ export const getRecentAppointmentList = async () => {
 
     const counts = (appointments.documents as Appointment[]).reduce(
       (acc, appointment) => {
-        if (appointment.status === "scheduled") {
-          acc.scheduledCount += 1;
-        } else if (appointment.status === "pending") {
-          acc.pendingCount += 1;
-        } else if (appointment.status === "cancelled") {
-          acc.cancelledCount += 1;
+        switch (appointment.status) {
+          case "scheduled":
+            acc.scheduledCount++;
+            break;
+          case "pending":
+            acc.pendingCount++;
+            break;
+          case "cancelled":
+            acc.cancelledCount++;
+            break;
         }
 
         return acc;
